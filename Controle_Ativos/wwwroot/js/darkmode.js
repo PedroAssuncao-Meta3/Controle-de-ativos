44bde583a37ad9f2e004abaa4bce280e472ce76c
PedroAssuncao-Meta3 <87086252+PedroAssuncao-Meta3@users.noreply.github.com>
2021-08-09 14:04:40 -0300

    Pequenas mudanças

--- Controle_Ativos/wwwroot/js/darkmode.js
+++ Controle_Ativos/wwwroot/js/darkmode.js
@@ -2,12 +2,13 @@ function toggleDarkLight() {
     //var body = document.getElementById("body").classList.toggle("dark-edition");
     let mode = localStorage.getItem('mode')
     if (mode == null || mode == 'light') {
-        document.getElementById("body").classList.toggle("dark-edition");
+        document.getElementById("body").classList.add("dark-edition");
+        document.getElementById("body").classList.remove("light-edition");
         localStorage.setItem('mode', 'dark')
     } else {
-        document.getElementById("body").classList.toggle("light-edition");
+        document.getElementById("body").classList.remove("dark-edition");
+        document.getElementById("body").classList.add("light-edition");
         localStorage.setItem('mode', 'light')
-        document.location.reload()
     }
 }
 
