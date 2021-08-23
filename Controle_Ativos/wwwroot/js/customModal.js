34cb54fce116cb36ad2724c76b602888537b9b0c
Marcus Vinicius <marcus.vinicius@meta3.com.br>
2021-08-23 10:17:52 -0300

    Modal

---
+++ Controle_Ativos/wwwroot/js/customModal.js
@@ -0,0 +1,12 @@
+﻿function ativarModal(elemento) {
+    let modal = document.getElementById(elemento.getAttribute('data-modal').replace('#', ''))
+    modal.classList.add('active')
+}
+
+function desativarModal(elemento) {
+    elemento.parentNode.classList.remove('active')
+}
+
+function desativarModalPeloId(id) {
+    document.getElementById(id).classList.remove('active')
+}
